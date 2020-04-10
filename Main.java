3cdcf576de7615636448724ed4a5625457fab482
ParrotGaming <jacobparrotoffical@gmail.com>
2020-04-09 18:34:38 -0700

    Made OSX compatible

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class gui {
     public static void main(String args[]) {
 
         //Creating the Frame
-        JFrame frame = new JFrame("To Do");
+        JFrame frame = new JFrame("Coffe Clicker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
 
@@ -20,6 +20,8 @@ class gui {
         JLabel l = new JLabel();
         l.setText("0");
         JPanel pan = new JPanel();
+        pan.add(l);
+        mb.add(pan);
 
         //Define button to get points
         JButton butt = new JButton("Get Beans");
@@ -29,6 +31,8 @@ class gui {
             public void actionPerformed (ActionEvent arg0) {
                     points = points +1;
                     l.setText(Integer.toString(points));
+                    pan.add(l);
+                    mb.add(pan);
                 }
             public int getpoints() {
                 return points;
@@ -41,13 +45,7 @@ class gui {
 
         //Adding Components to the frame.
         frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.SOUTH, butt);
         frame.setVisible(true);
-        frame.add(butt);
-
-        //Main loop
-        while(true) {
-            pan.add(l);
-            mb.add(pan); 
-        }
     }
 }
