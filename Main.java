8bb36624b37d7aaf84ecfc4f51d249bfcb1b186b
Parrot <jacobparrotoffical@gmail.com>
2020-04-09 17:39:32 -0700

    added points and button to collect them

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 class gui {
     public static void main(String args[]) {
-        int points = 0;
 
         //Creating the Frame
         JFrame frame = new JFrame("To Do");
@@ -13,19 +13,41 @@ class gui {
         JMenuBar mb = new JMenuBar();
         JMenu m1 = new JMenu("Store");
         mb.add(m1);
-        JMenuItem m11 = new JMenuItem("Breeder");
-        JMenuItem m22 = new JMenuItem("Aviary");
+        JMenuItem m11 = new JMenuItem("Upgrade Grinder");
         m1.add(m11);
-        m1.add(m22);
 
+        //Making the points visible
         JLabel l = new JLabel();
-        l.setText("label text");
+        l.setText("0");
         JPanel pan = new JPanel();
-        pan.add(l); 
+
+        //Define button to get points
+        JButton butt = new JButton("Get Beans");
+
+        class MyCoolActionListener implements ActionListener {
+            int points = 0;
+            public void actionPerformed (ActionEvent arg0) {
+                    points = points +1;
+                    l.setText(Integer.toString(points));
+                }
+            public int getpoints() {
+                return points;
+            }
+        }
+
+        MyCoolActionListener al = new MyCoolActionListener();
+        butt.addActionListener(al);
+        al.getpoints();
 
         //Adding Components to the frame.
         frame.getContentPane().add(BorderLayout.NORTH, mb);
         frame.setVisible(true);
-        frame.add(pan);
+        frame.add(butt);
+
+        //Main loop
+        while(true) {
+            pan.add(l);
+            mb.add(pan); 
+        }
     }
 }
